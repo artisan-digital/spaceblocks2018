1dd5f3607fa4b14913f7fdb46041f5da75f875bd
artisan.digital <25208440+artisan-digital@users.noreply.github.com>
2018-10-20 21:34:54 -0500

    Update geturltest.js

--- geturltest.js
+++ geturltest.js
@@ -12,7 +12,6 @@
  *You should have received a copy of the GNU General Public License
  *along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-(function(ext) {
 
   (function(ext) {
     // Cleanup function when the extension is unloaded
